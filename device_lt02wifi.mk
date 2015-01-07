6987c967a61b7d1c50edd69eaa80377e1d7c4291
laufersteppenwolf <laufersteppenwolf@gmail.com>
2015-01-08 00:12:48 +0100

    Switch to our own gps config

--- device_lt02wifi.mk
+++ device_lt02wifi.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/samsung/lt02wifi/lt02wifi-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/lt02wifi/overlay
